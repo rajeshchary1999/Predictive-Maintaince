e0e21ba364bf21543ac7d12bac940bee9a0debfa
GOTTOJU RAJESH <104690664+rajeshchary1999@users.noreply.github.com>
2022-11-13 11:43:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.ensemble import AdaBoostClassifier
+import sklearn
+
+st.title('PREDICTIVE MAINTENANCE')
+st.header('Domain : Aerospace')
+st.subheader('The main goal is to predict the remaining useful life (RUL) of each engine.')
+st.subheader('RUL is equivalent of number of flights remained for the engine after the last data point in the test dataset.')
+# import the model
+st.subheader('Enter The Sensor Measurement ValueS')
+ada = pickle.load(open('ada.pkl','rb'))
+data = pickle.load(open('data.pkl','rb'))
+
+# sensor 14
+sensor_14 = int(st.number_input('enter the sensor_14 value'))
+
+# sensor 3
+sensor_3 = st.number_input('enter the sensor_3 value')
+# sensor 9
+sensor_9 = st.number_input('enter the sensor_9 value')
+
+# sensor 7
+sensor_7 = st.number_input('enter the sensor_7 value')
+
+# sensor 11
+sensor_11 = st.number_input('enter the sensor_11 value')
+
+# sensor 21
+sensor_21 = st.number_input('enter the sensor_21 value')
+
+# sensor 15
+sensor_15 = st.number_input('enter the sensor_15 value')
+
+# sensor 12
+sensor_12 = st.number_input('enter the sensor_12 value')
+# sensor 4
+sensor_4 = st.number_input('enter the sensor_4 value')
+
+
+#if st.button('Predict RUL'):
+    #columns = [sensor_14,sensor_3,sensor_9,sensor_7,sensor_11,sensor_21,sensor_15,sensor_12,sensor_4]
+    #features = np.array(columns).reshape((len(columns), 1))
+    ##final_features = [np.array(features)]
+   # prediction = ada.predict(features)
+    #output = round(prediction[0], 2)
+    #st.title("The predicted price of this configuration is " + str(int(np.exp(ada.predict(output)[0]))))
+
+if st.button('Predict Price'):
+    query = np.array([sensor_14,sensor_3,sensor_9,sensor_7,sensor_11,sensor_21,sensor_15,sensor_12,sensor_4])
+    query = query.reshape(1, 9)
+    st.title("The Remaining Useful Life Is  " + str(int(ada.predict(query)[0])))
+
+st.write("GitHuB Link [link](https://github.com/rajeshchary1999)")
+
+
